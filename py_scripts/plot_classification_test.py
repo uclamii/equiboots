6c7bea7354fad264b4998178d27b9d5114e541d1
lshpaner <lshpaner@ucla.edu>
2025-04-08 12:16:58 -0700

    (+) addtl' plot testing scripts for debugging

---
+++ py_scripts/plot_classification_test.py
@@ -0,0 +1,52 @@
+import numpy as np
+import pandas as pd
+import equiboots as eqb
+
+
+if __name__ == "__main__":
+    # Generate synthetic test data
+    y_prob = np.random.rand(1000)
+    y_pred = y_prob > 0.5
+    y_true = np.random.randint(0, 2, 1000)
+
+    race = (
+        np.random.RandomState(3)
+        .choice(["white", "black", "asian", "hispanic"], 1000)
+        .reshape(-1, 1)
+    )
+    sex = np.random.choice(["M", "F"], 1000).reshape(-1, 1)
+
+    fairness_df = pd.DataFrame(
+        data=np.concatenate((race, sex), axis=1), columns=["race", "sex"]
+    )
+
+    # Initialize and process groups
+    eq = eqb.EquiBoots(
+        y_true=y_true,
+        y_prob=y_prob,
+        y_pred=y_pred,
+        fairness_df=fairness_df,
+        fairness_vars=["race", "sex"],
+    )
+    eq.grouper(groupings_vars=["race", "sex"])
+    sliced_data = eq.slicer("race")
+
+    eqb.eq_plot_group_curves(
+        sliced_data,
+        curve_type="pr",
+        # filename="bla",
+        title="ROC AUC by Race Group",
+        n_bins=10,
+        # group="black",
+        # exclude_groups=["black", "asian"],
+    )
+
+    eqb.eq_plot_group_curves(
+        sliced_data,
+        curve_type="roc",
+        # filename="bla",
+        title="ROC AUC by Race Group",
+        n_bins=10,
+        # group="black",
+        # exclude_groups=["black", "asian"],
+    )
