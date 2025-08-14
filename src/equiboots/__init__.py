88db24039321dea5b1e4f646109dbcf865536f11
lshpaner <lshpaner@ucla.edu>
2025-08-14 14:13:46 -0700

    Version up to 0.0.1a

--- src/equiboots/__init__.py
+++ src/equiboots/__init__.py
@@ -23,7 +23,7 @@ deployment.
 PyPI: https://pypi.org/project/equiboots  
 Documentation: https://uclamii.github.io/equiboots/
 
-Version: 0.0.0a10
+Version: 0.0.1a
 """
 
 
@@ -31,7 +31,7 @@ Version: 0.0.0a10
 __doc__ = detailed_doc
 
 
-__version__ = "0.0.0a10"
+__version__ = "0.0.1a"
 __author__ = "Leonid Shpaner, Arthur Funnell, Al Rahrooh, Panayiotis Petousis"
 __email__ = "lshpaner@ucla.edu; alafunnell@gmail.com; arahrooh@ucla.edu; pp89@ucla.edu"
 
